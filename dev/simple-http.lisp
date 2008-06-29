4e07d96f952dcb2f84ccd6adb0552ca3d4575958
Gary King <gwking@metabang.com>
2008-06-29 15:42:06 -0400

    some cleanup and refactoring and the big shift from trivial-socket to usocket
    darcs-hash:20080629194206-3cc5d-831015492bac236324302ebcc0149b9bd7180916.gz

--- dev/simple-http.lisp
+++ dev/simple-http.lisp
@@ -37,8 +37,12 @@
 		     (stream-from c) (stream-to c)))))
 
 (define-condition mismatched-download-size-error (download-error)
-                  ((length-claimed :initarg :length-claimed :reader download-length-claimed)
-                   (length-downloaded :initarg :length-downloaded :reader download-length-downloaded))
+                  ((length-claimed 
+		    :initarg :length-claimed
+		    :reader download-length-claimed)
+                   (length-downloaded
+		    :initarg :length-downloaded
+		    :reader download-length-downloaded))
   (:report (lambda (c s)
 	     (format s "There was problem when downloading ~A. The header claimed size was ~D but only downloaded ~D"
 		     (download-url c)
@@ -49,38 +53,72 @@
 ;;; http commands
 ;;; ---------------------------------------------------------------------------
 
+(defun write-standard-headers (command url host stream)
+  (write-string command stream)
+  (write-char #\Space stream)
+  (write-string url stream)
+  (write-string " HTTP/1.0" stream)
+  (write-crlf stream)
+  (write-string "Host: " stream)
+  (write-string host stream)
+  (write-crlf stream)
+  (write-string "User-Agent: " stream)
+  (write-string *user-agent* stream)
+  (write-crlf stream)
+  (write-string "Accept: " stream)
+  (write-string "*/*" stream)
+  (write-additional-headers stream)
+  (write-crlf stream)
+  (write-crlf stream)
+  (force-output stream))
+
 (defun http-head (url)
   "Returns a list of two elements: a response code as an integer and an association list of headers returned from the server."
   (let* ((host (url-host url))
          (port (url-port url))
-         (stream (open-stream host port)))
-    (format stream "HEAD ~A HTTP/1.0~AHost: ~A~AUser-Agent: simple HTTP for Common Lisp~A~A"
-	    url +crlf+ host +crlf+ +crlf+ +crlf+)
-    (force-output stream)
-    (prog1
-	(list
-	 (response-read-code stream)
-	 (response-read-headers stream))
-      (close stream))))
+	 (socket (socket-connect host port))
+	 (stream (socket-stream socket)))
+    (unwind-protect
+	 (progn
+	   (write-standard-headers "HEAD" url host stream)
+	   (prog1
+	       (list
+		(response-read-code stream)
+		(response-read-headers stream))))
+      (socket-close socket))))
 
 (defun http-get (url)
   "returns a list of three elements: a response code as integer, an association list of headers returned from the server, and a stream from which the response can be read."
   (let* ((host (url-host url))
          (port (url-port url))
-         (stream (open-stream host port)))
-    (format stream "GET ~A HTTP/1.0~AHost: ~A~AUser-Agent: simple HTTP for Common Lisp~A~A"
-	    url +crlf+ host +crlf+ +crlf+ +crlf+)
-    (force-output stream)
+	 (socket (socket-connect host port))
+	 (stream (socket-stream socket)))
+    (write-standard-headers "HEAD" url host stream)
     (list
      (response-read-code stream)
      (response-read-headers stream)
      stream)))
 
+;; as extensible as mud
+(defun write-additional-headers (stream)
+  (when (and *proxy-password* *proxy-user*)
+    (write-crlf stream)
+    (write-string "Proxy-Authorization: Basic" stream)
+    (write-string (base64-encode
+		   (format nil "~A:~A" *proxy-user* *proxy-password*)) stream)))
+
+(defun write-crlf (stream)
+  (write-char (code-char 13) stream)
+  (write-char (code-char 10) stream))
+
 (defun http-post (url content-type content &key headers (debug? *http-debug*))
-  "given a URL, a MIME content type, and the content as a character stream, POST to the URL and return the list of three elements as described for HTTP-GET."
+  "given a URL, a MIME content type, and the content as a character 
+stream, POST to the URL and return the list of three elements as 
+described for [http-get][]."
   (let* ((host (url-host url))
          (port (url-port url))
-         (http-stream (open-stream host port))
+	 (socket (socket-connect host port))
+	 (http-stream (socket-stream socket))
 	 (stream http-stream))
     (when debug?
       (setf stream (make-broadcast-stream stream debug?)))
@@ -88,7 +126,8 @@
     (when headers
       (loop for (n . v) in headers do
 	   (format stream "~A: ~A~A" n v +crlf+)))
-    (format stream "Content-Type: ~A~AContent-Length: ~D~A~A~A" content-type +crlf+ (length content) +crlf+ +crlf+ content)
+    (format stream "Content-Type: ~A~AContent-Length: ~D~A~A~A" 
+	    content-type +crlf+ (length content) +crlf+ +crlf+ content)
     (force-output stream)
     (list
      (response-read-code http-stream)
@@ -97,59 +136,89 @@
 
 (defun http-resolve (url &key (http-method 'http-get)
 		     (signal-error? t) (verbose? nil))
-  "Similar to http-get, http-resolve returns a list of four elements: the HTTP response code, the headers, the stream and the reslved URL. HTTP-response resolves 301 and 302 responses, and signals an error on responses greater than 400. If there is not an error, then the caller is responsible for closing the HTTP stream."
-  (handler-case 
-      (destructuring-bind (response headers stream)
-	  (block got
-	    (loop
-	       (destructuring-bind (response headers &optional stream)
-		   (funcall http-method url)
-		 (when verbose? 
-		   (format *debug-io* "~% ~A -> ~A" url response))
-		 (unless (member response '(301 302))	       
-		   (return-from got (list response headers stream)))
-		 (when stream 
-		   (close stream))
-		 (setf url (header-value :location headers)))))
-	(when (>= response 400)
-	  (when stream (close stream))
-	  (when signal-error?
+  "Similar to [http-get][], `http-resolve` returns a list of four 
+elements: the HTTP response code, the headers, the stream 
+and the resolved URL. HTTP-response resolves 301 and 302 
+responses, and signals an error on responses greater 
+than 400. If there is not an error, then the caller is responsible
+for closing the HTTP stream."
+  (block handler
+    (handler-case 
+	(destructuring-bind (response headers stream)
+	    (block got
+	      (loop
+		 (destructuring-bind (response headers &optional stream)
+		     (funcall http-method url)
+		   (when verbose? 
+		     (format *debug-io* "~% ~A -> ~A" url response))
+		   (unless (member response '(301 302))	       
+		     (return-from got (list response headers stream)))
+		   (when stream 
+		     (close stream))
+		   (setf url (header-value :location headers)))))
+	  (when (>= response 400)
+	    (when stream (close stream))
 	    (error 'download-error 
-		   :url url :command http-method :response response)))
-	(list response headers stream url))
-    (error (c)
-      (when signal-error?
-	(error c))
-      (list nil nil nil nil))))
+		   :url url :command http-method :response response))
+	  (list response headers stream url))
+      (error (c)
+	(when signal-error?
+	  (error c))
+	(list nil nil nil nil)
+	#+(or)
+	(unless signal-error?
+	  (return-from handler (list nil nil nil nil)))))))
 
-(defun http-download (url destination)
-  "Resolves `url` using http-resolve and downloads the contents of the stream it to `destination`. Destination is assumed to be a file. Returns (as multiple values) the number of elements downloaded (e.g., bytes) and the actual URL."
+(defun http-download (url destination &key (signal-error? t))
+  "Resolves `url` using http-resolve and downloads the contents of the 
+stream it to `destination`. Destination is assumed to be a file. 
+Returns \(as multiple values\) the number of elements downloaded 
+\(e.g., bytes\) and the actual URL."
   ;; mostly from ASDF-Install
   (destructuring-bind (response headers stream actual-url)
-                      (http-resolve url)
+      (http-resolve url :signal-error? signal-error?)
     (declare (ignore response))
-    (unwind-protect
-    (let ((length (parse-integer (or (header-value :content-length headers) "")
-				 :junk-allowed t))
-          (total 0))
-      #+:clisp (setf (stream-element-type stream)
-		     '(unsigned-byte 8))
-      (let ((ok? nil) (o nil))
-	(unwind-protect
-	     (progn
-	       (setf o (apply #'open destination
-			      :direction :output :if-exists :supersede
-			      (open-file-arguments)))
-	       (setf total (copy-stream stream o))
-	       (when length
-		 (unless (= length total)
-		   (error 'mismatched-download-size-error
-			  :length-claimed length
-			  :length-downloaded total)))
-	       (setf ok? t)
-	       (values total actual-url))
-	  (when o (close o :abort (null ok?))))))
-      (close stream))))
+    (when stream
+      (unwind-protect
+	   (let ((length (parse-integer 
+			  (or (header-value :content-length headers) "")
+			  :junk-allowed t))
+		 (total 0))
+	     #+:clisp (setf (stream-element-type stream)
+			    '(unsigned-byte 8))
+	     (let ((ok? nil) (o nil))
+	       (unwind-protect
+		    (progn
+		      (setf o (apply #'open destination
+				     :direction :output :if-exists :supersede
+				     (open-file-arguments)))
+		      (setf total (copy-stream stream o))
+		      (when length
+			(unless (= length total)
+			  (error 'mismatched-download-size-error
+				 :length-claimed length
+				 :length-downloaded total)))
+		      (setf ok? t)
+		      (values total actual-url))
+		 (when o (close o :abort (null ok?))))))
+	(close stream)))))
+
+(defun download-stream (stream destination &key expected-length)
+  (let ((retrieved 0))
+    (list
+     (metatilities:with-output (out destination
+				    :direction :output :if-exists :supersede)
+       (unwind-protect
+	    (progn
+	      (copy-stream stream out)
+	      (when expected-length
+		(unless (= expected-length retrieved)
+		  (error 'mismatched-download-size-error
+			 :length-claimed expected-length
+			 :length-downloaded retrieved))))
+	 ;; cleanup
+	 (close stream)))
+     retrieved)))
 
 ;;; ---------------------------------------------------------------------------
 ;;; 'utilities'
@@ -173,7 +242,9 @@
   (assert (string-equal url "http://" :end1 7))
   (let ((path-start (position #\/ url :start 7)))
     (let ((port-start (position #\: url :start 7 :end path-start)))
-      (if port-start (parse-integer url :start (1+ port-start) :junk-allowed t) 80))))
+      (if port-start
+	  (parse-integer url :start (1+ port-start) :junk-allowed t)
+	  80))))
 
 (defun url-host (url)
   (assert (string-equal url "http://" :end1 7))
@@ -224,7 +295,6 @@ Returns the total number of 'elements' read and written."
         (write-sequence buf to :end pos)))))
 
 ;; this next method stolen from Araneida
-
 (defun url-reserved-character-p (c)
   (not (or (member c '(#\- #\_ #\. #\! #\~ #\* #\' #\( #\) ))
            (alphanumericp c))))
@@ -239,11 +309,15 @@ Returns the total number of 'elements' read and written."
          collect (string c))))
 
 (defun header-value (name headers)
-  "Searchers headers for name _without_ case sensitivity. Headers should be an alist mapping symbols to values; name a symbol. Returns the value if name is found or nil if it is not."
+  "Searchers headers for name _without_ case sensitivity. Headers 
+should be an alist mapping symbols to values; name a symbol. 
+Returns the value if name is found or nil if it is not."
   (cdr (header-pair name headers)))
 
 (defun header-pair (name headers)
-  "Searchers headers for name _without_ case sensitivity. Headers should be an alist mapping symbols to values; name a symbol. Returns the \(name value\) pair if name is found or nil if it is not."
+  "Searches headers for name _without_ case sensitivity. Headers 
+should be an alist mapping symbols to values; name a symbol. 
+Returns the \(name value\) pair if name is found or nil if it is not."
   (assoc name headers 
          :test (lambda (a b) 
                  (string-equal (symbol-name a) (symbol-name b)))))
--- simple-http.asd
+++ simple-http.asd
@@ -1,22 +1,40 @@
 ;;;; Silly emacs, this is -*- Lisp -*- (or thereabouts)
 (in-package #:common-lisp-user)
 
-(defpackage #:simple-http-asdf 
+(defpackage #:simple-http-system 
   (:use #:common-lisp #:asdf))
 
-(in-package #:simple-http-asdf)
+(in-package #:simple-http-system)
 
 ;;this is necessary due to a bug in SBCL
 #+sbcl
 (require :sb-bsd-sockets)
 
 (defsystem simple-http
-    :name "simple-http"
-    :author "Brian Mastenbrook"
-    :licence "MIT"
-    :description "Simple support for HTTP GET and POST."
-    :depends-on (:trivial-sockets)
-    :components ((:module "dev"
-                          :components ((:file "simple-http")
-                                       
-                                       (:static-file "notes.text")))))
+  :name "simple-http"
+  :author "Brian Mastenbrook and Gary King"
+  :licence "MIT"
+  :description "Simple support for HTTP GET, POST and more."
+  :version "1.1.0"
+  :depends-on (:usocket)
+  :components ((:module 
+		"dev"
+		:components ((:file "package")
+			     (:file "variables"
+				    :depends-on ("package"))
+			     (:file "simple-http" 
+				    :depends-on ("variables"))
+			     (:file "base64"
+				    :depends-on ("variables"))
+			     (:static-file "notes.text"))))
+  :in-order-to ((test-op (load-op simple-http-test)))
+  :perform (test-op :after (op c)
+		    (funcall
+		      (intern (symbol-name '#:run-tests) :lift)
+		      :config :generic))
+  :depends-on ())
+
+(defmethod operation-done-p 
+           ((o test-op)
+            (c (eql (find-system 'simple-http))))
+  (values nil))
