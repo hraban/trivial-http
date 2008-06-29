4fabcb07ab830e9722db9d08342d6d972d039977
Gary King <gwking@metabang.com>
2008-06-29 15:44:29 -0400

    Added some proposed patches to patches -- these aren't used yet but I don't want to lose track of them
    darcs-hash:20080629194429-3cc5d-e3e3598ddaf49910bafb21741d45c014545a5b79.gz

--- pastes/download-images.lisp
+++ pastes/download-images.lisp
@@ -2,51 +2,50 @@
 ;;; download images
 
 (defun skip-headers (stream)
-  "Skips bytes in the stream until it runs into two CR-LF sequences"
-  (let* ((sequence '(13 10 13 10))
-         (point sequence))
-    (loop while point
-          do (if (= (read-byte stream) (car point))
-                 (setf point (cdr point))
-                 (setf point sequence))
-          finally (return stream))))
-     
+  "Skips bytes in the stream until it runs into two CR-LF sequences"
+  (let* ((sequence '(13 10 13 10))
+         (point sequence))
+    (loop while point
+       do (if (= (read-byte stream) (car point))
+	      (setf point (cdr point))
+	      (setf point sequence))
+       finally (return stream))))
+     
 
 (defun call-server (url method &key (element-type 'character) content-type content)
-  (let* ((host (url-host url))
-         (port (url-port url))
-         (stream (open-stream host port :element-type element-type)))
-    (flet ((send (string)
-             (if (eq element-type 'character)
-                 (princ string stream)
-                 (loop for char across string
-                       do (write-byte (char-code char) stream)))))
+  (let* ((host (url-host url))
+         (port (url-port url))
+         (stream (open-stream host port :element-type element-type)))
+    (flet ((send (string)
+             (if (eq element-type 'character)
+                 (princ string stream)
+                 (loop for char across string
+		    do (write-byte (char-code char) stream)))))
 
-      (send (format nil
-                    "~A ~A HTTP/1.0~AHost: ~A~AUser-agent: Trivial HTTP for Common Lisp~A"
-                    method url +crlf+ host +crlf+ +crlf+))
-      (if (eq method :post)
-          (send (format nil
-                        "Content-type: ~A~AContent-Length: ~D~A~A~A"
-                        content-type +crlf+ (length content) +crlf+ +crlf+ content))
-          (send +crlf+))
-      (force-output stream)
-      (if (eq element-type 'character)
-          (list
-           (response-read-code stream)
-           (response-read-headers stream)
-           stream)
-          (skip-headers stream)))))
-
+      (send (format nil
+                    "~A ~A HTTP/1.0~AHost: ~A~AUser-agent: Trivial HTTP for Common Lisp~A"
+                    method url +crlf+ host +crlf+ +crlf+))
+      (if (eq method :post)
+          (send (format nil
+                        "Content-type: ~A~AContent-Length: ~D~A~A~A"
+                        content-type +crlf+ (length content) +crlf+ +crlf+ content))
+          (send +crlf+))
+      (force-output stream)
+      (if (eq element-type 'character)
+          (list
+           (response-read-code stream)
+           (response-read-headers stream)
+           stream)
+          (skip-headers stream)))))
 
 (defun http-get (url)
-  (destructuring-bind (code headers stream)
-      (call-server url :head)
-    (close stream)
-    (let ((answer-type (assoc :content-type headers)))
-      (if (and answer-type (not (string= (cdr answer-type) "text/" :end1 5)))
-          (list code headers (call-server url :get :element-type '(unsigned-byte 8)))
-          (call-server url :get)))))
-    
+  (destructuring-bind (code headers stream)
+      (call-server url :head)
+    (close stream)
+    (let ((answer-type (assoc :content-type headers)))
+      (if (and answer-type (not (string= (cdr answer-type) "text/" :end1 5)))
+          (list code headers (call-server url :get :element-type '(unsigned-byte 8)))
+          (call-server url :get)))))
+    
 (defun http-post (url content-type content)
-  (call-server url :post :content-type content-type :content content))
+  (call-server url :post :content-type content-type :content content))
--- pastes/handling-headers.lisp
+++ pastes/handling-headers.lisp
@@ -2,120 +2,120 @@
 ;;; handling headers
 
 (defpackage :trivial-http
-  (:use :cl :trivial-sockets)
-  (:nicknames :thttp)
-  (:export :http-get :http-post :escape-url-query))
+  (:use :cl :trivial-sockets)
+  (:nicknames :thttp)
+  (:export :http-get :http-post :escape-url-query))
 (in-package :trivial-http)
 
 (defparameter *user-agent* "Trivial HTTP for Common Lisp")
 
 (defun url-port (url)
-  (assert (string-equal url "http://" :end1 7))
-  (let ((path-start (position #\/ url :start 7)))
-    (let ((port-start (position #\: url :start 7 :end path-start)))
-      (if port-start (parse-integer url :start (1+ port-start) :junk-allowed t) 80))))
+  (assert (string-equal url "http://" :end1 7))
+  (let ((path-start (position #\/ url :start 7)))
+    (let ((port-start (position #\: url :start 7 :end path-start)))
+      (if port-start (parse-integer url :start (1+ port-start) :junk-allowed t) 80))))
 
 (defun url-host (url)
-  (assert (string-equal url "http://" :end1 7))
-  (let* ((port-start (position #\: url :start 7))
-         (host-end (min (or (position #\/ url :start 7) (length url))
-                        (or port-start (length url)))))
-    (subseq url 7 host-end)))
+  (assert (string-equal url "http://" :end1 7))
+  (let* ((port-start (position #\: url :start 7))
+         (host-end (min (or (position #\/ url :start 7) (length url))
+                        (or port-start (length url)))))
+    (subseq url 7 host-end)))
 
 (defconstant +crlf+
-  (if (boundp '+crlf+)
-      (symbol-value '+crlf+)
-      (concatenate 'string
-                   (string (code-char 13))
-                   (string (code-char 10)))))
+  (if (boundp '+crlf+)
+      (symbol-value '+crlf+)
+      (concatenate 'string
+                   (string (code-char 13))
+                   (string (code-char 10)))))
 
 (defun response-read-code (stream)
-  (let* ((l (read-line stream))
-            (space (position #\Space l)))
-       (parse-integer l :start (1+ space) :junk-allowed t)))
+  (let* ((l (read-line stream))
+            (space (position #\Space l)))
+       (parse-integer l :start (1+ space) :junk-allowed t)))
 
 (defun response-read-headers (stream)
-  (loop for line = (read-line stream nil nil)
-     until (or (eql (length line) 0)
-               (eql (elt line 0) (code-char 13))
-               (eql (elt line 0) (code-char 10)))
-     collect
-       (let ((colon (position #\: line)))
-         (cons (intern (string-upcase (subseq line 0 colon)) :keyword)
-               (string-trim (list #\Space (code-char 13) (code-char 10))
-                            (subseq line (1+ colon)))))))
+  (loop for line = (read-line stream nil nil)
+     until (or (eql (length line) 0)
+               (eql (elt line 0) (code-char 13))
+               (eql (elt line 0) (code-char 10)))
+     collect
+       (let ((colon (position #\: line)))
+         (cons (intern (string-upcase (subseq line 0 colon)) :keyword)
+               (string-trim (list #\Space (code-char 13) (code-char 10))
+                            (subseq line (1+ colon)))))))
 
 (defun http-get (url &key headers connection)
-  "headers is an alist"
-  (let* ((host (url-host url))
-         (port (url-port url))
-         (http-headers (add-headers
-                        `(("Host" . ,host) ("User-Agent" . ,*user-agent*))
-                        headers :overwrite nil))
-         (stream (if connection
-                     connection
-                     (open-stream host port))))
+  "headers is an alist"
+  (let* ((host (url-host url))
+         (port (url-port url))
+         (http-headers (add-headers
+                        `(("Host" . ,host) ("User-Agent" . ,*user-agent*))
+                        headers :overwrite nil))
+         (stream (if connection
+                     connection
+                     (open-stream host port))))
 
-    ;; Send the request
-    (format stream "GET ~A HTTP/1.0~A" url +crlf+)
-    (write-headers http-headers stream)
-    (format stream "~A" +crlf+)        ; write-headers writes a trailing +crlf+
-    (force-output stream)
+    ;; Send the request
+    (format stream "GET ~A HTTP/1.0~A" url +crlf+)
+    (write-headers http-headers stream)
+    (format stream "~A" +crlf+)        ; write-headers writes a trailing +crlf+
+    (force-output stream)
 
-    ;; Read in the response
-    (list
-     (response-read-code stream)
-     (response-read-headers stream)
-     stream)))
+    ;; Read in the response
+    (list
+     (response-read-code stream)
+     (response-read-headers stream)
+     stream)))
 
 (defun http-post (url content-type content &key headers connection)
-  (let* ((host (url-host url))
-         (port (url-port url))
-         (http-headers (add-headers
-                        `(("Host" . ,host) ("User-Agent" . ,*user-agent*)
-                          ("Content-Type" . ,content-type)
-                          ("Content-Length" . ,(length content)))
-                        headers :overwrite nil))
-         (stream (if connection
-                     connection
-                     (open-stream host port))))
-    
-    ;; Send the request
-    (format stream "POST ~A HTTP/1.0~A" url +crlf+)
-    (write-headers http-headers stream)
-    (format stream "~A" +crlf+) ; write-headers writes a trailing +crlf+
-    (format stream "~A" content)
-    (force-output stream)
+  (let* ((host (url-host url))
+         (port (url-port url))
+         (http-headers (add-headers
+                        `(("Host" . ,host) ("User-Agent" . ,*user-agent*)
+                          ("Content-Type" . ,content-type)
+                          ("Content-Length" . ,(length content)))
+                        headers :overwrite nil))
+         (stream (if connection
+                     connection
+                     (open-stream host port))))
+    
+    ;; Send the request
+    (format stream "POST ~A HTTP/1.0~A" url +crlf+)
+    (write-headers http-headers stream)
+    (format stream "~A" +crlf+) ; write-headers writes a trailing +crlf+
+    (format stream "~A" content)
+    (force-output stream)
 
-    ;; Read in the response
-    (list
-     (response-read-code stream)
-     (response-read-headers stream)
-     stream)))
+    ;; Read in the response
+    (list
+     (response-read-code stream)
+     (response-read-headers stream)
+     stream)))
 
 ;; this next method stolen from Araneida
 
 (defun url-reserved-character-p (c)
-  (not (or (member c '(#\- #\_ #\. #\! #\~ #\* #\' #\( #\) ))
-           (alphanumericp c))))
+  (not (or (member c '(#\- #\_ #\. #\! #\~ #\* #\' #\( #\) ))
+           (alphanumericp c))))
 
 (defun escape-url-query (query)
-  (apply #'concatenate 'string
-   (loop for c across query
-         if (url-reserved-character-p c)
-         collect (format nil "%~2,'0X" (char-code c))
-         else
-         collect (string c))))
+  (apply #'concatenate 'string
+   (loop for c across query
+         if (url-reserved-character-p c)
+         collect (format nil "%~2,'0X" (char-code c))
+         else
+         collect (string c))))
 
 (defun write-headers (headers stream)
-  (dolist (header headers)
-    (format stream "~A: ~A~A" (car header) (cdr header) +crlf+)))
+  (dolist (header headers)
+    (format stream "~A: ~A~A" (car header) (cdr header) +crlf+)))
 
 (defun add-headers (new-headers header-list &key (overwrite t))
-  (dolist (header new-headers)
-    (if (assoc (first header) header-list :test #'string=)
-        (when overwrite
-          (setf (cdr (assoc (car header) header-list :test #'string=))
-                (cdr header)))
-        (push header header-list)))
-  header-list) 
+  (dolist (header new-headers)
+    (if (assoc (first header) header-list :test #'string=)
+        (when overwrite
+          (setf (cdr (assoc (car header) header-list :test #'string=))
+                (cdr header)))
+        (push header header-list)))
+  header-list) 
