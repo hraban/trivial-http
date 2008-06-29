8c58d6cf57634e1414af2b9c9b6ab11eb4a3fe69
Gary King <gwking@metabang.com>
2008-06-29 15:38:23 -0400

    Moved package definition into its own file - take 2
    darcs-hash:20080629193823-3cc5d-27f171fdd3022810521c046c658103c293f7f985.gz

---
+++ dev/package.lisp
@@ -0,0 +1,36 @@
+(in-package #:common-lisp-user)
+
+(defpackage #:simple-http
+  (:use #:cl #:usocket)
+  (:nicknames #:shttp)
+  (:export
+   #:*http-debug*
+   #:http-get 
+   #:http-post 
+   #:http-head
+   #:http-download
+   #:http-resolve
+   #:escape-url-query
+           
+   #:header-value
+   #:header-pair
+           
+   ;; conditions
+   #:download-error
+   #:download-url
+   #:download-command
+   #:download-response
+
+   #:incompatible-stream-error
+   #:stream-from
+   #:stream-to
+
+   #:mismatched-download-size-error
+   #:download-length-claimed
+   #:download-length-downloaded
+   #:*user-agent*)
+  (:documentation 
+   "simple-http is a simple networking library for doing HTTP POST 
+and GET over a socket interface. It establishes a package simple-HTTP, 
+also called SHTTP, from which the following functions are exported: 
+http-get, http-post, escape-url-query and http-head."))
